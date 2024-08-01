54f28673ec4f424009043c393dc12c1383c88695
Marios Spanakakis <mariosspanakakis@web.de>
2024-08-01 16:04:07 +0200

    add test case for obtaining piece information and generating pieces

--- tests/board_test.cpp
+++ tests/board_test.cpp
@@ -2,6 +2,50 @@
 #include "board.h"
 #include "movegen.h"
 
+
+TEST(BoardTest, GetPieceInformation) {
+    using namespace chess;
+    // get piece type
+    ASSERT_EQ(typeOf(WHITE_PAWN), PAWN);
+    ASSERT_EQ(typeOf(WHITE_KNIGHT), KNIGHT);
+    ASSERT_EQ(typeOf(WHITE_BISHOP), BISHOP);
+    ASSERT_EQ(typeOf(WHITE_ROOK), ROOK);
+    ASSERT_EQ(typeOf(WHITE_QUEEN), QUEEN);
+    ASSERT_EQ(typeOf(WHITE_KING), KING);
+    ASSERT_EQ(typeOf(BLACK_PAWN), PAWN);
+    ASSERT_EQ(typeOf(BLACK_KNIGHT), KNIGHT);
+    ASSERT_EQ(typeOf(BLACK_BISHOP), BISHOP);
+    ASSERT_EQ(typeOf(BLACK_ROOK), ROOK);
+    ASSERT_EQ(typeOf(BLACK_QUEEN), QUEEN);
+    ASSERT_EQ(typeOf(BLACK_KING), KING);
+    // get piece color
+    ASSERT_EQ(colorOf(WHITE_PAWN), WHITE);
+    ASSERT_EQ(colorOf(WHITE_KNIGHT), WHITE);
+    ASSERT_EQ(colorOf(WHITE_BISHOP), WHITE);
+    ASSERT_EQ(colorOf(WHITE_ROOK), WHITE);
+    ASSERT_EQ(colorOf(WHITE_QUEEN), WHITE);
+    ASSERT_EQ(colorOf(WHITE_KING), WHITE);
+    ASSERT_EQ(colorOf(BLACK_PAWN), BLACK);
+    ASSERT_EQ(colorOf(BLACK_KNIGHT), BLACK);
+    ASSERT_EQ(colorOf(BLACK_BISHOP), BLACK);
+    ASSERT_EQ(colorOf(BLACK_ROOK), BLACK);
+    ASSERT_EQ(colorOf(BLACK_QUEEN), BLACK);
+    ASSERT_EQ(colorOf(BLACK_KING), BLACK);
+    // make piece
+    ASSERT_EQ(makePiece(WHITE, PAWN), WHITE_PAWN);
+    ASSERT_EQ(makePiece(WHITE, KNIGHT), WHITE_KNIGHT);
+    ASSERT_EQ(makePiece(WHITE, BISHOP), WHITE_BISHOP);
+    ASSERT_EQ(makePiece(WHITE, ROOK), WHITE_ROOK);
+    ASSERT_EQ(makePiece(WHITE, QUEEN), WHITE_QUEEN);
+    ASSERT_EQ(makePiece(WHITE, KING), WHITE_KING);
+    ASSERT_EQ(makePiece(BLACK, PAWN), BLACK_PAWN);
+    ASSERT_EQ(makePiece(BLACK, KNIGHT), BLACK_KNIGHT);
+    ASSERT_EQ(makePiece(BLACK, BISHOP), BLACK_BISHOP);
+    ASSERT_EQ(makePiece(BLACK, ROOK), BLACK_ROOK);
+    ASSERT_EQ(makePiece(BLACK, QUEEN), BLACK_QUEEN);
+    ASSERT_EQ(makePiece(BLACK, KING), BLACK_KING);
+}
+
 TEST(BoardTest, SetAndUnsetPiece) {
     chess::Board originalBoard = chess::Board();
     chess::Board board = chess::Board();
